348f746b9efcc3b1764912741d08ee4476afc274
marlon-frazao <frazao.marlon@gmail.com>
2021-04-16 07:41:08 -0300

    first commit

---
+++ src/main/java/com/marlonfrazao/desafiozup/DesafioZupApplication.java
@@ -0,0 +1,13 @@
+package com.marlonfrazao.desafiozup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlonfrazao/desafiozup/DesafioZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlonfrazao.desafiozup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
